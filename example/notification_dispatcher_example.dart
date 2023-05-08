c226493d4896771c1aef029c1643dfee86fddc04
Loong Yeat <pongloongyeat@gmail.com>
2023-05-08 20:17:30 +0800

    Revert "chore: Use Flutter example"
    This reverts commit 609cb4abf3a0900047105bbe58f4eca06d800423.

---
+++ example/notification_dispatcher_example.dart
@@ -0,0 +1,25 @@
+import 'package:notification_dispatcher/notification_dispatcher.dart';
+
+class YourClass {
+  int count = 0;
+
+  void init() {
+    NotificationDispatcher.instance.addObserver(
+      this,
+      name: 'observerName',
+      callback: (_) => count++,
+    );
+  }
+
+  void dispose() {
+    NotificationDispatcher.instance.removeObserver(this);
+  }
+}
+
+void main() {
+  final yourClass = YourClass()..init();
+  NotificationDispatcher.instance.post(name: 'observerName');
+
+  print(yourClass.count); // 1
+  yourClass.dispose();
+}
