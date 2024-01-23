daf2a71b9154840e9b0d25b5be7fe4a50b989eae
Pong Loong Yeat <pongloongyeat@gmail.com>
2024-01-24 00:03:41 +0800

    feat: Add flutter_notification_observer

---
+++ flutter_notification_observer/lib/flutter_notification_observer.dart
@@ -0,0 +1,5 @@
+library flutter_notification_observer;
+
+export 'src/multi_notification_observer.dart';
+export 'src/notification_observer.dart';
+export 'package:notification_dispatcher/notification_dispatcher.dart';
